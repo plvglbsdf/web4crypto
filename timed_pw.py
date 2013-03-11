0070f31c0d7a83cef40db3e0fcf3dd9c3bd521b7
Marek Majkowski <majek04@gmail.com>
2013-03-11 22:20:51 +0000

    move back... also, _urandom is called if possible therefore whole exercise was impossible

--- timed_pw.py
+++ timed_pw.py
@@ -3,19 +3,23 @@
 import string
 import time
 import random
+import bottle
+import os
+
 
 # random uses int(time.time() * 256) when it's first time loaded
 # make sure we call random.choice soon after
 SECRET_LEN = 12
 
 secret_time = time.time()
+
+# this is the default for systems without _urandom, btw.
+random.seed(int(secret_time * 256))
+
 secret = ''.join(random.choice(string.ascii_letters) for i in range(SECRET_LEN))
 print("[.] Secret_time: %r %r" % (secret_time, int(secret_time * 256)))
 print("[.] Secret: %r" % (secret,))
 
-
-import bottle
-import os
 
 
 
