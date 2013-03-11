36047ea4d0255a62de00729e2b081e720d8eab48
Marek Majkowski <majek04@gmail.com>
2013-03-11 21:08:11 +0000

    Final adaptations. Make the success message more exciting :)

--- timed_pw.py
+++ timed_pw.py
@@ -17,22 +17,18 @@ print("[.] Secret: %r" % (secret,))
 @bottle.route('/')
 @bottle.get('/login')
 def login_form(message = ''):
-    if reveal_password:
-        shown_pw = pw
-    else:
-        shown_pw = ''
-    announce = message +\
-            '<p>Password of length {0} was generated {1} seconds ago. {2}</p>'.\
-            format(length, int(time.time()-start_time), shown_pw)
+    announce = message + \
+            '<p>Password of length {0} was generated {1} seconds ago.</p>'.\
+            format(length, int(time.time() - secret_time))
     return announce + form
 
-@bottle.post('/login') # or @route('/login', method='POST')
+@bottle.post('/login')
 def login_submit():
     password = bottle.request.forms.get('password')
     if password == pw:
-        return '''<p>Your login was correct.</p>'''
+        return '''<p>Your password was CORRECT! How's that possible! OMG!</p>'''
     else:
-        return '''<p>Your login was incorrect; try again.</p>''' + form
+        return '''<p>Your password was incorrect; try again.</p>''' + form
 
 
 bottle.run(host='0.0.0.0', port=8080, debug=True)
