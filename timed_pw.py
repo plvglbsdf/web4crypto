70c89274396b4ccd2cf7dad0fa0d30ddc1c314f6
Marek Majkowski <majek04@gmail.com>
2013-03-11 20:58:49 +0000

    Spurious comments

--- timed_pw.py
+++ timed_pw.py
@@ -1,29 +1,12 @@
 #!/usr/bin/env python3.2
-# timed_pw.py
-# 20130311
-# David Prager Branner
 # Run with Python 3.2
- 
-"""Creates preliminary page for Marek Majkowski's Crypto demonostration.
-Generates a password;
-reports whole seconds elapsed since password creation;
-reports length of password;
-prompts user to enter password, repeating until correct.
-
-Options: on command line, user may enter 
-    * an integer for optional password length (default is 10)
-    * flag -p to display password
-"""
 
 import time
 import random
 import bottle
 import sys
 
-# Preliminary settings
-# Check command line flags and options
 length = 10
-reveal_password = False
 for i in sys.argv:
     try:
         length = int(i)
@@ -38,9 +21,8 @@ form = '''<form method="POST" action="/login">
 start_time = 0
 pw = ''
 
-# HTTP Methods
 @bottle.route('/')
-@bottle.get('/login') # or @route('/login')
+@bottle.get('/login')
 def login_form(message = ''):
     if reveal_password:
         shown_pw = pw
@@ -59,10 +41,7 @@ def login_submit():
     else:
         return '''<p>Your login was incorrect; try again.</p>''' + form
 
-# Begin preparing password
 start_time = time.time()
-# Printable ASCII ranges from 32 to 126
 pw = ''.join([chr(random.randint(32, 126)) for i in range(length)])
-#
-# Run server
+
 bottle.run(host='0.0.0.0', port=8080, debug=True)
